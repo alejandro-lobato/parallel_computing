2fdb4ccbeafca63faf4e7842196de03b3ac77a8a
Alejandro Lobato Molina <lobatoma@MacBook-Pro-de-Alejandro.local>
2020-02-05 16:08:07 -0600

    Added initial files for the project.

---
+++ P1/code/convolution.c
@@ -0,0 +1,19 @@
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+#include "lib/stb_image.h"
+#include "lib/stb_image_write.h"
+#include <stdio.h>
+
+int main(){
+    //Define kernel.
+    //Read the image file.
+    //Convert image file.
+    //Apply convolution.
+    //Write image to file.
+    //??
+    //Profit.
+
+    return 0;
+}
