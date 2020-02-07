0b664a036ec0e50cb7270738bbd4a3216b79090a
Alejandro Lobato Molina <lobatoma@MacBook-Pro-de-Alejandro.local>
2020-02-06 19:06:56 -0600

    Included report and lines of code for the algorithm.

--- P1/code/convolution.c
+++ P1/code/convolution.c
@@ -9,17 +9,36 @@
 int main(){
 
     //Read the image file.
-    int x,y,n;
-    unsigned char *data = stbi_load("img/lena256.png", &x, &y, &n, 1);
+    int width,height,n;
+    unsigned char* data = stbi_load("img/lena256.png", &width, &height, &n, 1);
     
+    //Create the pointer with the data to create the image from.
+    unsigned char* output_data;
+
     //Define kernel.
-    
-    
-    //Convert image file.
+    double kernel[9] = {0.0625, 0.125, 0.0625, 0.125, 0.25, 0.125, 0.0625, 0.125, 0.0625};
+
     //Apply convolution.
+    int i = 0;
+    int neighborhood[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    //For each pixel, we'll multiply.
+    for(i = 0; i < height * width; i++){
+        printf("%i ", data[i]);
+        output_data[i] = 0;
+        int neighbor = 0;
+        for(int j = 0; j < 9; i++){
+            //Sum that shit boi.
+
+            //If it's in the left side
+            if(i*width==0){
+
+            }
+        }
+    }
 
     //Write image to file.
-    stbi_write_png("img/greylena256.png", x, y, 1, data, 256);
+    stbi_write_png("img/greylena256.png", width, height, 1, data, 256);
     stbi_image_free(data);
     //??
     //Profit.
